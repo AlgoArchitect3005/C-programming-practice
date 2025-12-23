29ff0537ea6ef199f8b60a73c2067d28f7b04875
AlgoArchitect3005 <guptayash.programmer@gmail.com>
2025-12-24 00:27:20 +0530

    To demonstrate use of realloc() function

---
+++ Dynamic memory allocation/UseofRealloc.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Define a structure to represent a point in 2D space
+struct Point {
+    int x;
+    int y;
+};
+
+int main(){
+    int n, i;
+    struct Point *ptr;
+    printf("Enter number of points: ");
+    scanf("%d", &n);
+    ptr = (struct Point*)calloc(n, sizeof(struct Point));
+    if(ptr == NULL){
+        printf("Memory not allocated");
+        return 1;
+    }
+    // Input coordinates for each point
+    for(i=0; i<n; i++){
+        printf("Enter x and y coordinates for point %d: ", i+1);
+        scanf("%d %d", &ptr[i].x, &ptr[i].y);
+    }
+    // Display the points
+    printf("The points are:\n");
+    for(i=0; i<n; i++){
+        printf("Point %d: (%d, %d)\n", i+1, ptr[i].x, ptr[i].y);
+    }
+    free(ptr);
+
+    // Reallocate memory to hold double the number of points
+    printf("Enter new number of points: ");
+    scanf("%d", &n);
+    ptr=(struct Point*)realloc(ptr, n*sizeof(struct Point));
+    if(ptr == NULL){
+        printf("Memory not reallocated");
+        return 1;
+    }
+    // Input coordinates for each point
+    for(i=0; i<n; i++){
+        printf("Enter x and y coordinates for point %d: ", i+1);
+        scanf("%d %d", &ptr[i].x, &ptr[i].y);
+    }
+    // Display the points
+    printf("The points are:\n");
+    for(i=0; i<n; i++){
+        printf("Point %d: (%d, %d)\n", i+1, ptr[i].x, ptr[i].y);
+    }
+    free(ptr);
+    return 0;
+}
