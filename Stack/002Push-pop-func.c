c3cee8c7ba40573042f2e20f90f95dd841150c96
AlgoArchitect3005 <guptayash.programmer@gmail.com>
2026-01-13 18:45:52 +0530

    Add push and popat_index functions to stack implementation

--- Stack/002Push-pop-func.c
+++ Stack/002Push-pop-func.c
@@ -6,6 +6,8 @@ struct Stack {
     int size;
     int* arr;
 };
+//case 1:
+//call by reference
 void push(struct Stack* s1 ,int value){
     if(s1->top == s1->size -1){
         printf("Stack overflow\n");
@@ -16,6 +18,20 @@ void push(struct Stack* s1 ,int value){
         printf("Pushed %d onto stack \n",s1->arr[s1->top]);
     }
 }
+//case 2:
+//call by reference
+void popat_index (struct Stack* s2, int index){
+    if(s2->top ==-1){
+        printf("Stack underflow\n");
+    }
+    elseif(index <0 || index > s2->top){
+        printf("Invalid index\n");
+    }
+    else{
+        printf("Pooped elements at index %d is: %d \n",index,s2->arr[index]);
+    }
+}
+
 int main(){
     // Create a stack of size 5
     struct Stack* s = (struct Stack*)malloc(sizeof(struct Stack));
