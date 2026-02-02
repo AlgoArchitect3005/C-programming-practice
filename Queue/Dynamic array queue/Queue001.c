8d91a6f56c59faad0f63a9c81807caea285a2c22
Yash Gupta <guptayash.programmer@gmail.com>
2026-02-02 23:51:43 +0530

    added enqueue method

--- Queue/Dynamic array queue/Queue001.c
+++ Queue/Dynamic array queue/Queue001.c
@@ -16,6 +16,19 @@ struct Queue initialiseQueue(struct Queue *q, int size){
     q->arr=(int*)malloc(q->size * sizeof(int));
     return *q;
 }
+struct Queue enqueue(struct Queue *q, int data){
+    if(q->front==-1 && q->rear==-1){
+        q->front++;
+        q->rear++;
+        q->arr[q->rear]=data;
+    }
+    else{
+        q->rear++;
+        q->arr[q-rear]=data;
+    }
+    printf("Enqueued element is %d\n",q->arr[q->rear]);
+    return *q;
+}
 
 int main(){
      struct Queue *q;
