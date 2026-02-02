e9dbb5d45d27b7db40a44fdd6ac24825eb424d41
Yash Gupta <guptayash.programmer@gmail.com>
2026-02-03 00:35:40 +0530

    improved logic in enqueue method

--- Queue/Dynamic array queue/Queue001.c
+++ Queue/Dynamic array queue/Queue001.c
@@ -2,31 +2,32 @@
 #include<stdlib.h>
 
 struct Queue{
-    
     int *arr;
     int front;
     int rear;
     int size;
-}
+};
 // Function to create a queue
-struct Queue initialiseQueue(struct Queue *q, int size){
+void initialiseQueue(struct Queue *q, int size){
     q->size = size;
     q->front = -1;
     q->rear=-1;
     q->arr=(int*)malloc(q->size * sizeof(int));
-    return *q;
 }
 // Function to add element to the queue
 struct Queue enqueue(struct Queue *q, int data){
-    if(q->front==-1 && q->rear==-1){
-        q->front++;
-        q->rear++;
-        q->arr[q->rear]=data;
-    }
-    else{
-        q->rear++;
-        q->arr[q->rear]=data;
-    }
+   if(q->rear==q->size-1){
+       printf("Queue is overflow\n");
+   }
+   else if(q->front==-1 && q->rear==-1){
+       q->front=0;
+       q->rear=0;
+       q->arr[q->rear]=data;
+   }
+   else{
+       q->rear++;
+       q->arr[q->rear]=data;
+   }
     printf("Enqueued element is %d\n",q->arr[q->rear]);
     return *q;
 }
