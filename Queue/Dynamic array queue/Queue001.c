a9a1d69c761a83a972392b63c4dc0fb2c7fb642f
Yash Gupta <guptayash.programmer@gmail.com>
2026-02-02 23:41:29 +0530

    created initialised queue method

--- Queue/Dynamic array queue/Queue001.c
+++ Queue/Dynamic array queue/Queue001.c
@@ -8,8 +8,17 @@ struct Queue{
     int rear;
     int size;
 }
+// Function to create a queue
+struct Queue initialiseQueue(struct Queue *q, int size){
+    q->size = size;
+    q->front = -1;
+    q->rear=-1;
+    q->arr=(int*)malloc(q->size * sizeof(int));
+    return *q;
+}
 
 int main(){
-     struct Queue q;
+     struct Queue *q;
+     q=initialiseQueue(q,2);
     return 0;
 }
