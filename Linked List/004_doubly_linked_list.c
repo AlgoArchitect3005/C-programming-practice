7d0cb07091f64f48013fd9ad27d74e535f93169d
Yash Gupta <guptayash.programmer@gmail.com>
2026-02-15 02:01:11 +0530

    refactor: reorganize and enhance reverse and traverse functions in doubly linked list

--- Linked List/004_doubly_linked_list.c
+++ Linked List/004_doubly_linked_list.c
@@ -5,6 +5,32 @@ struct Node {
     struct Node* next;
     struct Node* prev;
 };
+void space(){
+    printf("\n");
+}
+void reverse(struct Node** head){
+    struct Node *temp=NULL;
+    struct Node *current=*head;
+    while(current!=NULL){
+        temp = current->prev;
+        current->prev=current->next;
+        current->next=temp;
+        current = current->prev;
+    if(temp!=NULL)
+      *head = temp->prev;
+        
+    }
+}
+
+void traverse(struct Node* head){
+    struct Node* temp=head;
+    while(temp!=NULL)
+    // for(int i=0 ;i<=9;i++)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
 int main(){
     struct Node* head = NULL;
     struct Node* second = NULL;
@@ -28,23 +54,13 @@ int main(){
 
     third->next=NULL;
     third->prev=second;
+    
+    //
+    traverse(head);
+    space();
+    printf("\n");
+    reverse(&head);
+    space();
+    traverse(head);
     return 0;
-}
-void reverse(struct Node** head){
-    struct Node *temp=null;
-    struct Node *current=*head;
-    while(current!=NULL){
-        temp = current->prev;
-        current->prev=current->next;
-        current->next=temp;
-        current = current->prev;
-    }
-}
-
-void traverse(struct Node* head){
-    struct Node* temp=head;
-    while(temp!=NULL){
-        printf("%d ",temp->data);
-        temp=temp->next;
-    }
 }
